d89f34c7416b041b1b5cee7965721a9e615ff4e6
15dredr <15dredr@whs.whsat.uk>
2021-01-22 10:50:44 +0000

    added basic pygame functionality, drew a white rectangle which moves to the right

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("Hello World")
-print("Hello World")
+import pygame
+
+pygame.init()
+DISPLAY = pygame.display.set_mode((800, 600))
+FPSCLOCK = pygame.time.Clock()
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+left_edge = 50
+top = 50
+width = 10
+height = 40
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    DISPLAY.fill(black)
+    pygame.draw.rect(DISPLAY, white, (left_edge, top, width, height))
+    left_edge += 5
+    pygame.display.update()
+    FPSCLOCK.tick(30)
